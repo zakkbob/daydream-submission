88f69f50c3685e70bc3d5dc0e812a1ff008e1637
zakkbob <114868973+zakkbob@users.noreply.github.com>
2025-09-28 14:15:21 +0100

    Reset level on fall

--- daydream-submission/player.gd
+++ daydream-submission/player.gd
@@ -22,3 +22,7 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	get_tree().reload_current_scene()
